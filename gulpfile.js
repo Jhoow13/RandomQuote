840b68760f3208daa8025c09aaeaa9d18fdfb515
Joel Silva <joelcsilva@outlook.com>
2018-10-04 00:58:06 -0300

    Creating build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
+var clean = require('gulp-clean');
 var browserSync = require('browser-sync').create();
+var buildFiles = ['./app/index.html', './app/style.css','./app/scripts.js'];
+
+
 
 gulp.task('reload', function(done){
     browserSync.reload();
     done();
 });
+
+gulp.task('build', function(){
+  gulp.src(buildFiles)
+    .pipe(gulp.dest('./'));
+})
 
 gulp.task('serve', function(){
   browserSync.init({
